6bbf4ce63a606c9957b0949ec4ba3e94103c5d5f
FatherofElias <supra_3_8@hotmail.com>
2024-10-15 14:56:10 -0500

    update

--- digitalLibrary.js
+++ digitalLibrary.js
@@ -3,4 +3,8 @@ function Book(title, author, pages) {
     this.title = title;
     this.author = author;
     this.pages = pages;
+// Task 2
+    this.displayInfo = function() {
+        return `Title: ${this.title}, Author: ${this.author}, Pages: ${this.pages}`;
+    }
 }
